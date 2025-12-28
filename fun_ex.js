01abda7e998306f00a76ca91706b31302feb7d06
sivani2609 <vbsivani.sep26@gmail.com>
2025-12-28 23:43:12 +0530

    Add files via upload

---
+++ fun_ex.js
@@ -0,0 +1,10 @@
+var f_actor="vijay"
+var f_player="Dhoni"
+var f_movie="leo"
+
+function favourite(){
+    console.log("favourite actor:"+f_actor)
+    console.log("favourite player:"+f_player)
+    console.log("favourite movie:"+f_movie)
+}
+favourite()
